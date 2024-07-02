57f7cdcb0f30a0c19fbe5cc26aaa0ce5f6ba32dd
Ilya Maltsev <111198978+IlyaMaltsev92@users.noreply.github.com>
2024-07-02 10:57:01 +0200

    Update conftest.py

--- all_tests/conftest.py
+++ all_tests/conftest.py
@@ -20,17 +20,3 @@ def browser():
     chrome_browser.implicitly_wait(10)
 
     return chrome_browser
-
-
-# driver = browser()
-# driver.get("https://sbis.ru/contacts/")
-# region = driver.find_element('xpath',"//span[@class='sbis_ru-Region-Chooser__text sbis_ru-link']")
-# region.click()
-# kamchatka = driver.find_element('xpath',"//span[@title='Камчатский край']")
-# print(kamchatka)
-# time.sleep(10)
-# driver.execute_script("arguments[0].scrollIntoView(true);",div)
-# WebDriverWait(driver,20).until(EC.visibility_of_element_located(('xpath','//img[@alt="Сопровождаем клиентов"]')))
-# elements = driver.find_elements('xpath',"//img[@class='tensor_ru-About__block3-image new_lazy loaded']")
-# # time.sleep(2)
-# pprint(len(elements))
