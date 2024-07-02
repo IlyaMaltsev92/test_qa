868646fbf446547bbec5d89906785efa8fd4e346
Ilya Maltsev <111198978+IlyaMaltsev92@users.noreply.github.com>
2024-07-02 11:02:37 +0200

    Add files via upload

---
+++ scripts/scenario_script_3.py
@@ -0,0 +1,34 @@
+import time
+
+from pages.sbis_ru import sbis_MainPage,DownloadPage
+import os
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--incognito')
+# chrome_options.add_argument('--window-size=1920,1080')
+# service = Service(executable_path=ChromeDriverManager().install())
+#
+# browser = webdriver.Chrome(service=service, options=chrome_options)
+
+path = 'C:\\Users\ILYA\Downloads\sbisplugin-setup-web.exe'
+expected_size = 7570584 # bytes
+
+
+browser.get(sbis_MainPage.URL)
+main_page = sbis_MainPage(browser)
+local_versions = main_page.download_local_versions()
+
+browser.execute_script("arguments[0].scrollIntoView(true);", local_versions)
+main_page.click_download_local_versions()
+
+download_page = DownloadPage(browser)
+ref = download_page.download_button()
+download_page.click_download_button()
+
+time.sleep(10)
+file_size = os.path.getsize(path)
+assert expected_size == file_size, 'Wrong size'
+print('test_passed')
